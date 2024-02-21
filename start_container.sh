a33b775756db7bba4324befdb3fc0b1dd56eabf7
aaaditya1804 <147532381+aaaditya1804@users.noreply.github.com>
2024-02-21 20:59:10 +0530

    Updating repository details in CodeDeploy

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull "$DOCKER_REGISTRY_URL/$DOCKER_REGISTRY_USERNAME/simple-python-flask-app:latest"
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 aaaditya/simple-python-flask-app
